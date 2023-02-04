b2f3b90528e995dec94ce09a04301e8fcd19856a
Samir Sabri <as3now@gmail.com>
2023-02-04 14:49:49 +0300

    python local server

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,6 @@
 service nginx stop
 service nginx start
 cd /app
-RAILS_ENV=production bundle exec puma -C /app/config/puma.rb
+python3 -m http.server 3000 --bind 127.0.0.1
+#RAILS_ENV=production bundle exec puma -C /app/config/puma.rb
 /bin/bash
